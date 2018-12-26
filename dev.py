d023a9c0760654cc3c866a4f0c7fc653951e2ad9
Mejri Issam <infor_issam@yahoo.fr>
2018-12-26 19:40:03 +0000

    ajout

--- dev.py
+++ dev.py
@@ -2,4 +2,5 @@ import os
 os.system("echo ahmed")
 os.system("git add . && git commit -am 'ajout' ")
 os.system(" git pull origin master")
+os.system(" git push origin master")
 print("new branch test")
