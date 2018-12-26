fa0cd1340b17d37e4450cbe83ef67d95f07c17f0
Mejri Issam <infor_issam@yahoo.fr>
2018-12-26 19:49:16 +0000

    ajout

--- dev.py
+++ dev.py
@@ -1,6 +1,8 @@
 import os
+from sys import argv as ar
+arg1=ar[1]
 os.system("echo ahmed")
 os.system("git add . && git commit -am 'ajout' ")
-os.system(" git pull origin master")
-os.system(" git push origin master")
+os.system(" git pull origin arg1")
+os.system(" git push origin arg1")
 print("new branch test")
