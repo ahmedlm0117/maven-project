9d180f00a0ae01636482cb1b74a45317aeef5407
Mejri Issam <infor_issam@yahoo.fr>
2018-12-26 19:52:20 +0000

    ajout

--- dev.py
+++ dev.py
@@ -1,6 +1,6 @@
 import os
-from sys import argv as ar
-arg1=ar[1]
+import sys as s
+arg1=s.argv[1]
 os.system("echo ahmed")
 os.system("git add . && git commit -am 'ajout' ")
 os.system(" git pull origin arg1")
